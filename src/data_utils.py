330667c0ba61e95e5f0d386acba4d3308bc683f8
Mohammed Saqib <saqibm128@gmail.com>
2020-03-27 00:41:59 -0400

    some changes to deal with unevenly sampled data

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,1 +1,27 @@
-import 
+import pandas as pd
+import numpy as np
+import json, os
+
+config = None #global config object. lru cache is smarter tbh, but i like having a globally accessible variable to monkey around with like an idiot
+
+
+
+def read_config(path=os.path.realpath("../config.json") else os.environ["CONFIG_PATH"])):
+    global config
+    if config is None:
+        config = json.load(open(path, "rb"))
+    return config
+def cis_data_dir():
+    return read_config()["cis_pd_data_dir"]
+def real_pd_data_dir():
+    return read_config()["real_pd_data_dir"]
+def get_mongo_client():
+    '''
+    Used for Sacred to record results
+    '''
+    config = read_config()
+    if "mongo_uri" not in config.keys():
+        return pymongo.MongoClient()
+    else:
+        mongo_uri = config["mongo_uri"]
+        return pymongo.MongoClient(mongo_uri)
